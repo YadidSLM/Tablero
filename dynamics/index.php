ba91b689c247b9b96bbfef6cd19f5ba4ef250e9e
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2022-05-04 12:03:56 -0500

    Añadí maquetado y titulito3

--- dynamics/index.php
+++ dynamics/index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titulito3</title>
+</head>
+<body>
 <?php
 
 $size = $_POST["size"];
@@ -21,3 +30,5 @@ for ($i = 0; $i < $size; $i++) {
 echo "</table>";
 
 ?>
+</body>
+</html>
